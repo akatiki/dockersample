4c858190aa4ffdc0217cfc9736452b0ac2410939
akatiki <49416755+akatiki@users.noreply.github.com>
2019-07-15 11:22:41 -0700

    Add files via upload

---
+++ src/test/java/com/nextnav/dockersample/DockersampleApplicationTests.java
@@ -0,0 +1,16 @@
+package com.nextnav.dockersample;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DockersampleApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
